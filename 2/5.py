45f2d897a29bcb7cb73d8d9dda9baae7c774432f
devcom2020 <kwp@tlen.pl>
2022-08-01 15:57:54 +0200

    final poniedziałek 1.08

---
+++ 2/5.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+d = defaultdict(lambda: "nie występuje")
+d["a"] = 1
+d["b"] = 2
+
+
+
+print(d["a"])
+print(d["b"])
+# print(d.__missing__("a"))
+print(d["c"])
